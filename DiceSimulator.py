e31b2706d59c6e9013d2d052396b35e03ecf1529
Sahana-Chandrashekar <csahana765@gmail.com>
2020-05-11 14:57:39 +0530

    adding dice simulator in local repo

---
+++ DiceSimulator.py
@@ -0,0 +1,56 @@
+import random
+print("This is a Dice Simulator")
+x = "y"
+
+while x == "y":
+    number = random.randint(1,6)
+
+    if number ==1:
+        print("---------------------------------")
+        print("|                                          |")
+        print("|                                          |")
+        print("|                     0                   |")
+        print("|                                          |")
+        print("|                                          |")
+        print("---------------------------------")
+    if number == 2:
+        print("---------------------------------")
+        print("|                                          |")
+        print("|                    0                    |")
+        print("|                                          |")
+        print("|                    0                    |")
+        print("|                                          |")
+        print("---------------------------------")
+    if number == 3:
+        print("---------------------------------")
+        print("|                    0                    |")
+        print("|                                          |")
+        print("|                    0                    |")
+        print("|                                          |")
+        print("|                    0                    |")
+        print("---------------------------------")
+    if number == 4:
+        print("---------------------------------")
+        print("|                                          |")
+        print("|          0                 0           |")
+        print("|                                          |")
+        print("|          0                 0           |")
+        print("|                                          |")
+        print("---------------------------------")
+    if number == 5:
+        print("---------------------------------")
+        print("|                                          |")
+        print("|          0                 0           |")
+        print("|                    0                     |")
+        print("|          0                 0           |")
+        print("|                                          |")
+        print("---------------------------------")
+    if number == 6:
+        print("---------------------------------")
+        print("|          0                 0          |")
+        print("|                                          |")
+        print("|          0                 0          |")
+        print("|                                          |")
+        print("|          0                 0          |")
+        print("---------------------------------")
+    x = input("Enter y or n:   ") 
